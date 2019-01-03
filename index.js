586bfbd7290ae79ebcb91c6b3380d98908ad109b
Paul Morimoto <psmorimoto@gmail.com>
2019-01-03 13:40:09 -0800

    Renders header tourlisting and bookings on proxy

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const port = 3012;
 
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/:listingId', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, './public/index.html'))
+});
+
+
 
 app.listen(port, () => console.log(`listening on port ${port}...`));
