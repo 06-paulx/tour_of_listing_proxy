3aaf0c71625f32d93f9823375d5f5c81bc6e7932
Paul Morimoto <psmorimoto@gmail.com>
2019-01-03 16:47:36 -0800

    Render all services on proxy server

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const port = 3012;
+const port = 3015;
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
